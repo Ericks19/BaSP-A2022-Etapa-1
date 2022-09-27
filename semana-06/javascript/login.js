535cd91e899432ab5e0130f35799bf746d319a98
Erick Sequeira <ericknz2014@gmail.com>
2022-09-27 01:07:43 -0300

    BaSP-W06: work on validation to sign up

--- semana-06/javascript/login.js
+++ semana-06/javascript/login.js
@@ -1,72 +1,71 @@
 window.onload = function () {
-  // Email
+
+  var logInButton = document.querySelector ("#logInButton");
   var emailInput = document.querySelector ("#email-input")
-  console.log (emailInput);
+  var password = document.querySelector ("#input-password");
 
   var emailExpression = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
   var emailValid = false;
   var passwordValid = false;
+  var hasLetters = false;
+  var hasNumber = false;
+  var alphabet = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","ñ","z","x","c","v","b","n","m"];
+  var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 
-emailInput.addEventListener("blur", function() {
+  function verifyIfPasswordHasNumber (password){
+    for (var i = 0; i < numbers.length; i++) {
+      for (var j = 0; j < password.length; j++){
+        if (hasNumber === false) {
+          if (numbers [i]=== password [j]){
+            hasNumber = true
+  
+          }else {
+            hasNumber = false
+          }
+        }
+     } 
+    
+    } 
+    return hasNumber;
+  }
+
+  function verifyIfPasswordHasLetters (password){
+    for (var i = 0; i < alphabet.length; i++) {
+      for (var j = 0; j < password.length; j++){
+        if (hasLetters === false) {
+          hasLetters = alphabet[i] === password [j];
+        };
+     } 
+    
+    } 
+    // devuelve valor true o false
+    return hasLetters
+  }
+
+  emailInput.addEventListener("blur", function() {
   if (emailExpression.test(emailInput.value)) {
-    emailInput.classList.add("border-green");
-    emailValid = true;
-  }else {
-    emailInput.classList.add("border-red");
-    emailValid = false;
+      emailInput.classList.add("border-green");
+      emailValid = true;
+  } else {
+      emailInput.classList.add("border-red");
+      emailValid = false;
   }
 });
-emailInput.onfocus = function () {
+  emailInput.onfocus = function () {
   emailInput.classList.remove("border-green", "border-red");
   }
 
 // Password
-var password = document.querySelector ("#input-password");
-var hasLetters = false;
-var hasNumber = false;
-var alphabet = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","ñ","z","x","c","v","b","n","m"];
-var numbers = ["0","1","2","3","4","5","6","7","8","9"];
-function verifyIfPasswordHasNumber (password){
-  for (var i = 0; i < numbers.length; i++) {
-    for (var j = 0; j < password.length; j++){
-      if (hasNumber === false) {
-       /* hasNumber = numbers[i] === password [j];*/
-        if (numbers [i]=== password [j]){
-          hasNumber = true
-
-        }else {
-          hasNumber = false
-        }
-      }
-   } 
-  
-  } 
-  return hasNumber;
-}
-
-function verifyIfPasswordHasLetters (password){
-  for (var i = 0; i < alphabet.length; i++) {
-    for (var j = 0; j < password.length; j++){
-      if (hasLetters === false) {
-        hasLetters = alphabet[i] === password [j];
-      };
-   } 
-  
-  } 
-  // devuelve valor true o false
-  return hasLetters
-}
-
 password.addEventListener ("blur", function(){
 var passwordHasNumber = verifyIfPasswordHasNumber(password.value);
 var passwordHasLetters = verifyIfPasswordHasLetters (password.value);
   if (passwordHasNumber && passwordHasLetters){
-    password.classList.add("border-green");
-    passwordValid = true  
+      password.classList.add("border-green");
+      passwordValid = true  
   } else {
-    password.classList.add("border-red");
-    passwordValid = false
-  } 
+      password.classList.add("border-red");
+      passwordValid = false
+      } 
   hasLetters = false
   hasNumber = false
 })
@@ -76,7 +75,7 @@ password.onfocus = function () {
 
 //boton
 
-var logInButton = document.querySelector ("#logInButton");
+
 logInButton.addEventListener("click", function(e){
   e.preventDefault();
   if (emailValid && passwordValid){
--- semana-06/javascript/sign-up.js
+++ semana-06/javascript/sign-up.js
@@ -1,77 +1,223 @@
 window.onload = function () {
 
-var firstName = document.querySelector ("#first-name")
-var alphabet = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","ñ","z","x","c","v","b","n","m"];
+  var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+  var alphabet = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","ñ","z","x","c","v","b","n","m"];
 
-function validateFirstName(firstName) {
-  var firstNameHasLetters = false
-  
- /* for (var i = 0; i < firstName.length; i++) {
+  //inputs
+  var firstName = document.querySelector ("#first-name")
+  var lastName = document.querySelector ("#last-name")
+  var dni = document.querySelector ("#dni")
+  var bithdate = document.querySelector ("#bithdate")
+  var phone =  document.querySelector ("#phone")
+  var address =  document.querySelector ("#address")
+  var locality =  document.querySelector ("#locality")
+  var zipcode =  document.querySelector ("#zipcode")
+  var email =  document.querySelector ("#email")
+  var password =  document.querySelector ("#password")
+  var password2 =  document.querySelector ("#password2")
 
-      // si el alfabeto incluye la letra que estamos iterando
-      if(!alphabet.includes(firstName[i].toLowerCase())){
-          isValid =false;
-      }
-  }*/
-  for (var i = 0; i < alphabet.length; i++) {
-    for (var j = 0; j < firstName.length; j++){
-      if (firstNameHasLetters === false) {
-        firstNameHasLetters = alphabet[i] === firstName [j];
-      };
-   } 
-  
-  } 
+  //...
 
-  if (firstName.length > 3 && firstNameHasLetters) {
-      firstName.classList.add (".border-green")
-  } else {
-      firstName.classList.add ("border-red");
+  //si el valueToTest solo tiene números
+  function hasOnlyNumbers(valueToTest) {
+      for (var j = 0; j < valueToTest.length; j++){
+          if (isNaN(valueToTest[j])) { //Is Not a Number
+           return false
+          }
+        }
+      return true;
   }
-  
-}
+  //si el valueToTest solo tiene letras
+  function hasOnlyLetters(valueToTest) {
+      for (var j = 0; j < valueToTest.length; j++){
+          if (!isNaN(valueToTest[j])) { // ! es una negación
+           return false
+          }
+        }
+      return true;
+  }
+  function hasNumbers (valueToTest){
+      var hasNumber = false;
+      for (var i = 0; i < numbers.length; i++) {
+        for (var j = 0; j < valueToTest.length; j++){
+          if (hasNumber === false) {
+           /* hasNumber = numbers[i] === password [j];*/
+            if (numbers [i]=== valueToTest [j]){
+              hasNumber = true
 
-// telefono
-var phone = document.querySelector('#phone');
-var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+            }else {
+              hasNumber = false
+            }
+          }
+       }
 
-function hasOnlyNumbers(valueToTest) {
-    for (var j = 0; j < valueToTest.length; j++){
-      if (isNaN(valueToTest[j])) {
-       return false
       }
+      return hasNumber; //o es true o false
     }
-  return true;
-}
 
-function hasNumbers(valueToTest) {
+    function hasLetters (valueToTest){
+      var hasLetters = false;
+      for (var i = 0; i < alphabet.length; i++) {
+        for (var j = 0; j < valueToTest.length; j++){
+          if (hasLetters === false) {
+            hasLetters = alphabet[i] === valueToTest [j];
+          };
+       }
 
-}
+      }
+      // devuelve valor true o false
+      return hasLetters
+    }
 
-phone.addEventListener('blur', function() {
-  phone.classList.remove("border-green", "border-red") 
-  
-  if (phone.value.length == 10 && hasOnlyNumbers(phone.value)) {
-    phone.classList.add ("border-green")
-  }else {
-    phone.classList.add ("border-red");
+  function hasValidEmail(valueToTest) {
+      var emailExpression = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
+      if(emailExpression.test(valueToTest)) {
+          return true;
+      }
+      return false
   }
-})
+  function hasValidDateformat(valueToTest) {
+      // Con formato dd/mm/aaaa.
+      var dateToTest = valueToTest.split('/');
+      var dateday   =  dateToTest[0]
+      var dateMonth =  dateToTest[1]
+      var dateYear  = dateToTest[2]
+      if(dateday >= 1 && dateday <= 31 &&  dateMonth >= 1 && dateMonth <= 12 && dateYear.length == 4) {
+          return true
+      }
+      return false
+  }
+
+  function hasMiddleSpace(valueToTest) {
+      //checar letra por letra y verificar que alguna de las letras, que no sea la primera ni la ultima sean un espacio
+      for (var i = 1; i < valueToTest.length-1; i++) {
+          //valueToTest[i] es el caractr actual de la palabra enviada, dentor del ciclo
+          if(valueToTest[i] === " ") {
+              return true
+          }
+      }
+      return false;
+
+  }
+  function hasValidAddress(valueToTest) {
+      //Al menos 5 caracteres con letras, números y un espacio en el medio.
+      //calle 1234 ->
+      if(hasNumbers(valueToTest) && hasLetters(valueToTest) && valueToTest.length >=5 && hasMiddleSpace(valueToTest)) {
+          return true;
+      }
+      return false;
+  }
+                              //EJERCICIOS
 
 
 
+  // Nombre: Solo letras y debe tener más de 3 letras.
+  firstName.addEventListener('blur', function() {
+      //al inicio quito los bordes para reiniciar la validación
+      firstName.classList.remove("border-green", "border-red")
+      if(hasOnlyLetters(firstName.value) && firstName.value.length >= 3) {
+          firstName.classList.add("border-green")
+      } else {
+          firstName.classList.add("border-red")
+      }
+  })
+  // Apellido: Solo letras y debe tener más de 3 letras.
+  lastName.addEventListener('blur', function() {
+      //al inicio quito los bordes para reiniciar la validación
+      lastName.classList.remove("border-green", "border-red")
+      if(hasOnlyLetters(lastName.value) && lastName.value.length >= 3) {
+          lastName.classList.add("border-green")
+      } else {
+          lastName.classList.add("border-red")
+      }
+  })
 
+  // DNI: Solo número y debe tener más de 7 números.
+  dni.addEventListener('blur', function() {
+      dni.classList.remove("border-green", "border-red")
+      if(hasOnlyNumbers(dni.value) && dni.value.length>7) {
+          dni.classList.add("border-green")
+      } else {
+          dni.classList.add("border-red")
+      }
+  })
 
+  // Fecha de Nacimiento: Con formato dd/mm/aaaa.
+  bithdate.addEventListener('blur', function() {
+      bithdate.classList.remove("border-green", "border-red")
+      if(hasValidDateformat(bithdate.value)) {
+          bithdate.classList.add("border-green")
+      } else {
+          bithdate.classList.add("border-red")
+      }
+  })
 
+  // Teléfono: Solo número y debe tener 10 números.
+  phone.addEventListener('blur', function() {
+      phone.classList.remove("border-green", "border-red")
+      if(hasOnlyNumbers(phone.value) && phone.value.length == 10) {
+          phone.classList.add("border-green")
+      } else {
+          phone.classList.add("border-red")
+      }
+  })
 
+  // Localidad:  Texto alfanumérico y debe tener más de 3 letras.
+  locality.addEventListener('blur', function() {
+      locality.classList.remove("border-green", "border-red")
+      if(hasLetters(locality.value) && hasNumbers(locality.value) && locality.value.length > 3) {
+          locality.classList.add("border-green")
+      } else {
+          locality.classList.add("border-red")
+      }
+  })
 
+  // Dirección: Al menos 5 caracteres con letras, números y un espacio en el medio.
+  address.addEventListener('blur', function() {
+      address.classList.remove("border-green", "border-red")
+      if(hasValidAddress(address.value)) {
+          address.classList.add("border-green")
+      } else {
+          address.classList.add("border-red")
+      }
+  })
+  // Código Postal: Solo número y debe tener entre 4 y 5 números.
+  zipcode.addEventListener('blur', function() {
+      zipcode.classList.remove("border-green", "border-red")
+      if(hasOnlyNumbers(zipcode.value) && zipcode.value.length >=4 && zipcode.value.length <=5) {
+          zipcode.classList.add("border-green")
+      } else {
+          zipcode.classList.add("border-red")
+      }
+  })
+  // Email: Debe tener un formato de email válido.
+  email.addEventListener('blur', function() {
+      email.classList.remove("border-green", "border-red")
+      if(hasValidEmail(email.value)) {
+          email.classList.add("border-green")
+      } else {
+          email.classList.add("border-red")
+      }
+  })
+  // Contraseña: Al menos 8 caracteres, formados por letras y números.
+  password.addEventListener('blur', function() {
+      password.classList.remove("border-green", "border-red")
+      if(hasLetters(password.value) && hasNumbers(password.value) && password.value.length >= 8) {
+          password.classList.add("border-green")
+      } else {
+          password.classList.add("border-red")
+      }
+  })
 
-
-
-
-
-
-
-
+  //Repetir Contraseña: Al menos 8 caracteres, formados por letras y números.
+  password2.addEventListener('blur', function() {
+      password2.classList.remove("border-green", "border-red")
+      if(hasLetters(password2.value) && hasNumbers(password2.value) && password2.value.length >= 8) {
+          password2.classList.add("border-green")
+      } else {
+          password2.classList.add("border-red")
+      }
+  })
 
 
 
